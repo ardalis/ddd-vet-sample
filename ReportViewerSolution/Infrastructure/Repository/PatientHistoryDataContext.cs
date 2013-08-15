0c15a04aee15c9efebe09953e0c8f33afcc46bac
Julie Lerman <julielerman@gmail.com>
2013-08-15 14:00:33 -0400

    Committing again because git won't let me pull yet.

---
+++ ReportViewerSolution/Infrastructure/Repository/PatientHistoryDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using PatientHistory.Domain;
+
+
+
+namespace Repository
+{
+  public class PatientHistoryDataContext:DbContext
+  {
+    public IDbSet<PatientInfo> Patients { get; set; }
+  
+  }
+}
---
+++ ReportViewerSolution/TestsAndSpecs/TestsAndSpecs/Helpers/DBSeedingInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using PatientHistory.Domain;
+using Repository;
+
+namespace TestsAndSpecs.Helpers
+{
+  public class PatientHistoryDBSeedingInitializer:DropCreateDatabaseAlways<PatientHistoryDataContext>
+  {
+    protected override void Seed(PatientHistoryDataContext context)
+{
+  context.Patients.Add
+    (PatientInfo.CreatePatientInfoWithHistory(0,"Sampson", "Flynn", "Newfoundland", "Dog", 120,DateTime.Now,5, "watch tremor in rear legs")); 
+      
+      base.Seed(context);
+}
+     
+
+  }
+}
