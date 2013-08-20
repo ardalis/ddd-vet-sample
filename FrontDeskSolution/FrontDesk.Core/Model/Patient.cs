208aa883c8cd16d3bc4ad0704d722cc6f3594a64
Steve Smith <steve@kentsmiths.com>
2013-08-19 20:40:57 -0400

    Initial Solution for Front Desk

---
+++ FrontDeskSolution/FrontDesk.Core/Model/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontDesk.Core.Model
+{
+    public class Client
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Salutation { get; set; }
+        public string PreferredName { get; set; }
+    }
+}
---
+++ FrontDeskSolution/FrontDesk.UnitTests/Model/ClientShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FrontDesk.Core.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FrontDesk.UnitTests.Model
+{
+    [TestClass]
+    public class ClientShould
+    {
+        [TestMethod]
+        public void HaveAFewNameProperties()
+        {
+            var client = new Client();
+            string firstName = "Test First Name";
+            string lastName = "Test Last Name";
+            string salutation = "Mr.";
+            string preferredName = "Billy Joe";
+
+            client.FirstName = firstName;
+            client.LastName = lastName;
+            client.Salutation = salutation;
+            client.PreferredName = preferredName;
+
+            Assert.AreEqual(firstName, client.FirstName);
+            Assert.AreEqual(lastName, client.LastName);
+            Assert.AreEqual(salutation, client.Salutation);
+            Assert.AreEqual(preferredName, client.PreferredName);
+        }
+    }
+}
