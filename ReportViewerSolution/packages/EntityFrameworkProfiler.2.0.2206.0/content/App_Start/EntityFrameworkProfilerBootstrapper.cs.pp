e49427661bb5fc6616a4c363d38640de8196e10e
Julie Lerman <julielerman@gmail.com>
2013-08-27 16:31:56 -0400

    added efprof to PH.UnitTest using NuGet
    then removed the humongous TOOLING folder with the client app that is
    installed via nuget. builds, tests run on my computer. Crossing fingers
    for bild server :)

---
+++ ReportViewerSolution/packages/EntityFrameworkProfiler.2.0.2206.0/content/App_Start/EntityFrameworkProfilerBootstrapper.cs.pp
@@ -0,0 +1,21 @@
+﻿using HibernatingRhinos.Profiler.Appender.EntityFramework;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.EntityFrameworkProfilerBootstrapper), "PreStart")]
+namespace $rootnamespace$.App_Start
+{
+	public static class EntityFrameworkProfilerBootstrapper
+	{
+		public static void PreStart()
+		{
+			// Initialize the profiler
+			EntityFrameworkProfiler.Initialize();
+			
+			// You can also use the profiler in an offline manner.
+			// This will generate a file with a snapshot of all the EntityFramework activity in the application,
+			// which you can use for later analysis by loading the file into the profiler.
+			// var filename = @"c:\profiler-log";
+			// EntityFrameworkProfiler.InitializeOfflineProfiling(filename);
+		}
+	}
+}
+
---
+++ ReportViewerSolution/packages/EntityFrameworkProfiler.2.0.2206.0/content/App_Start/EntityFrameworkProfilerBootstrapper.vb.pp
@@ -0,0 +1,18 @@
+﻿Imports HibernatingRhinos.Profiler.Appender.EntityFramework
+
+<assembly: WebActivator.PreApplicationStartMethod(GetType(Global.$rootnamespace$.App_Start.EntityFrameworkProfilerBootstrapper), "PreStart")>
+Namespace App_Start
+	Public Class EntityFrameworkProfilerBootstrapper
+        Public Shared Sub PreStart()
+            ' Initialize the profiler
+			EntityFrameworkProfiler.Initialize()
+
+            ' You can also use the profiler in an offline manner.
+            ' This will generate a file with a snapshot of all the EntityFramework activity in the application,
+            ' which you can use for later analysis by loading the file into the profiler.
+            ' Dim FileName as String = @"c:\profiler-log";
+            ' EntityFrameworkProfiler.InitializeOfflineProfiling(FileName)
+        End Sub
+    End Class
+End Namespace
+
