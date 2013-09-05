72f78012bcbffc37b37b9afde0abd415516b768b
Steve Smith <steve@kentsmiths.com>
2013-09-05 12:05:19 -0400

    Working on UI and appointment creation

---
+++ FrontDeskSolution/FrontDesk.Web/Models/CreateAppointmentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FrontDesk.Web.Models
+{
+    public class CreateAppointmentViewModel
+    {
+        public Guid PatientId { get; set; }
+        public Guid ClientId { get; set; }
+        public DateTime DateOfAppointment { get; set; }
+        public TimeSpan Duration { get; set; }
+        public Guid SelectedDoctor { get; set; }
+        public SelectList Doctors { get; set; }
+        public string Details { get; set; }
+    }
+
+    public class DoctorViewModel
+    {
+        public Guid DoctorId { get; set; }
+        public string Name { get; set; }
+    }
+}
