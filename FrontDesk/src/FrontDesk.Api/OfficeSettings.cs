20c0217c640ed198b25bc676da6601af9b665583
ShadyNagy <info@shadynagy.com>
2020-09-21 18:56:48 +0200

    added edit icon and delete icon.

--- FrontDesk/src/FrontDesk.Api/OfficeSettings.cs
+++ FrontDesk/src/FrontDesk.Api/OfficeSettings.cs
@@ -6,6 +6,6 @@ namespace FrontDesk.Api
     public class OfficeSettings : IApplicationSettings
     {
         public int ClinicId { get { return 1; } }
-        public DateTime TestDate { get { return new DateTime(2030, 9, 23); } }
+        public DateTime TestDate { get { return new DateTime(2014, 6, 9); } }
     }
 }
