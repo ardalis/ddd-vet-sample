eaefd10328206f76fd81508f6257d61119b0b265
ShadyNagy <info@shadynagy.com>
2020-09-24 12:13:07 +0200

    Clean using.

--- FrontDesk/src/FrontDesk.Blazor/Shared/SchedulerComponent/SchedulerResourceModel.cs
+++ FrontDesk/src/FrontDesk.Blazor/Shared/SchedulerComponent/SchedulerResourceModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FrontDesk.Blazor.Shared.SchedulerComponent
+﻿namespace FrontDesk.Blazor.Shared.SchedulerComponent
 {
     public class SchedulerResourceModel
     {
